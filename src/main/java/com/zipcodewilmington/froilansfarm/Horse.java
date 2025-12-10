c634311e6126c814512a5e976f2ecc3ec4d1e6b9
mmcgough8 <mwm9490@icloud.com>
2025-12-10 09:18:31 -0500

    "Added AnimalTest and Horse Class"

--- src/main/java/com/zipcodewilmington/froilansfarm/Horse.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Horse.java
@@ -1,5 +1,48 @@
 package com.zipcodewilmington.froilansfarm;
 
-public class Horse extends Animal {
+import com.zipcodewilmington.froilansfarm.Animal;
+import com.zipcodewilmington.froilansfarm.Interfaces.Rideable;
+import com.zipcodewilmington.froilansfarm.Interfaces.Rider;
+
+public class Horse extends Animal implements Rideable {
     
+    private Rider currentRider;
+
+    public Horse(String name) {
+        super(name);
+        this.currentRider = null;
+    }
+
+    @Override
+    public String makeNoise() {
+        return "Neigh!";
+    }
+
+    @Override
+    public boolean mount(Rider rider) {
+        if (rider == null || currentRider != null) {
+            return false;
+        }
+        this.currentRider = rider;
+        return true;
+    }
+
+    @Override
+    public boolean dismount(Rider rider) {
+        if (currentRider == null || currentRider != rider) {
+            return false;
+        }
+        this.currentRider = null;
+        return true;
+    }
+
+    @Override
+    public boolean isMounted() {
+        return currentRider != null;
+    }
+
+    @Override
+    public Rider getCurrentRider() {
+        return currentRider;
+    }
 }
---
+++ src/test/java/com/zipcodewilmington/froilansfarm/AnimalTestSimple.java
@@ -0,0 +1,47 @@
+package com.zipcodewilmington.froilansfarm;
+
+import com.zipcodewilmington.froilansfarm.animals.Horse;
+import com.zipcodewilmington.froilansfarm.animals.Chicken;
+import com.zipcodewilmington.froilansfarm.crops.EarCorn;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AnimalTestSimple {
+    
+    @Test
+    public void testHorseName() {
+        Horse horse = new Horse("Spirit");
+        assertEquals("Spirit", horse.getName());
+    }
+    
+    @Test
+    public void testHorseStartsNotFed() {
+        Horse horse = new Horse("Spirit");
+        assertFalse(horse.hasBeenFed());
+    }
+    
+    @Test
+    public void testHorseCanEat() {
+        Horse horse = new Horse("Spirit");
+        horse.eat(new EarCorn());
+        assertTrue(horse.hasBeenFed());
+    }
+    
+    @Test
+    public void testHorseMakesNoise() {
+        Horse horse = new Horse("Spirit");
+        assertEquals("Neigh!", horse.makeNoise());
+    }
+    
+    @Test
+    public void testChickenMakesNoise() {
+        Chicken chicken = new Chicken("Henrietta");
+        assertEquals("Cluck!", chicken.makeNoise());
+    }
+    
+    @Test
+    public void testToString() {
+        Horse horse = new Horse("Spirit");
+        assertEquals("Horse named Spirit", horse.toString());
+    }
+}
