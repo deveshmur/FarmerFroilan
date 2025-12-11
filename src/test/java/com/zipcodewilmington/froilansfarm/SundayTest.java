e1fc4f4364fd33d79efaec7ed711d41131fa636f
b-rickert <bd.rickert52@gmail.com>
2025-12-11 14:58:35 -0500

    Sunday Test

--- src/test/java/com/zipcodewilmington/froilansfarm/SundayTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/SundayTest.java
@@ -1,5 +1,32 @@
 package com.zipcodewilmington.froilansfarm;
 
+import org.junit.Test;
+import com.zipcodewilmington.froilansfarm.Interfaces.CropRow;
+
 public class SundayTest {
-    
+
+    @Test
+    void wednesdayPlanting() {
+
+        MorningTest morning = new MorningTest();
+        morning.performMorningRoutine();
+
+        Farm farm = morning.farm;
+        Farmer froilan = (Farmer) farm.getFarmHouse().get(0);
+
+        farm.getField().addRow(new CropRow());
+        farm.getField().addRow(new CropRow());
+        farm.getField().addRow(new CropRow());
+        farm.getField().addRow(new CropRow());
+        farm.getField().addRow(new CropRow());
+        Field field = farm.getField();
+
+        froilan.plant(new CornStalk(), field.getRow(0));
+        froilan.plant(new TomatoPlant(), field.getRow(1));
+        froilan.plant(new CarrotPlant(), field.getRow(2));
+    }
 }
+
+
+
+
